bae7bf82b807dcfab15cc150ab0438f5da784df9
C. Spencer Beggs <spencer@beggs.codes>
2026-07-17 22:32:26 -0400

    chore: update deps
    Signed-off-by: C. Spencer Beggs <spencer@beggs.codes>

--- packages/demo/__test__/index.test.ts
+++ packages/demo/__test__/index.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from "vitest";
-import type { Foo } from "../src/index.ts";
-import { Bar } from "../src/index.ts";
+import type { Foo } from "../src/index.js";
+import { Bar } from "../src/index.js";
 
 describe("Bar class", () => {
 	it("should create an instance of Bar", () => {
